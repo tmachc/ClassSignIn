a4622d63d002b4b43aae4934e25be238a52b9f2b
tmachc <835892182@qq.com>
2016-05-11 23:56:43 +0800

    登录注册 键盘

--- ClassSignInSwift/ClassSignInSwift/LoginViewController.swift
+++ ClassSignInSwift/ClassSignInSwift/LoginViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 import SnapKit
 import Alamofire
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet var tfUserName: UITextField!
     @IBOutlet var tfPassword: UITextField!
+    @IBOutlet var scrView: UIScrollView! // 为了键盘弹起来的时候 页面上滑
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,14 +24,29 @@ class LoginViewController: UIViewController {
         
         // 定义所有子页面返回按钮的名称
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "返回", style: UIBarButtonItemStyle.Done, target: nil, action: nil)
+        
+        // 添加点击手势
+        let gesture = UITapGestureRecognizer.init(target: self, action: #selector(hideKeyboard))
+        self.view.addGestureRecognizer(gesture)
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBarHidden = true
+        
+        // 设置监听 当键盘将要隐藏的时候 发通知给（调用）self.willHideKeyboard() 函数
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(willHideKeyboard), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // 删除监听
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
     }
     
     @IBAction func clickLogin(sender: UIButton) {
+        self.hideKeyboard()
         // 先判断输入的是否为空
         if tfUserName.text == "" {
             
@@ -64,6 +80,32 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func clickToRegisterPage(sender: UIButton) {
+        self.hideKeyboard()
         self.performSegueWithIdentifier("register", sender: nil)
+    }
+    
+    // MARK: - 控制键盘
+    
+    func hideKeyboard() {
+        if tfPassword.isFirstResponder() || tfUserName.isFirstResponder() {
+            tfPassword.resignFirstResponder()
+            tfUserName.resignFirstResponder()
+        }
+    }
+    
+    // 开始编辑的时候 设置滚动view的大小为两倍屏幕高 然后向上滚动一段距离
+    func textFieldDidBeginEditing(textField: UITextField) {
+        scrView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height * 2)
+        scrView.setContentOffset(CGPointMake(0, 100), animated: true)
+    }
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        self.hideKeyboard()
+    }
+    
+    // 收起（隐藏）键盘的时候 设置滚动view为当前屏幕大小，回到 (0,0)
+    func willHideKeyboard() {
+        scrView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height)
+        scrView.setContentOffset(CGPointZero, animated: true)
     }
 }
--- ClassSignInSwift/ClassSignInSwift/RegisterViewController.swift
+++ ClassSignInSwift/ClassSignInSwift/RegisterViewController.swift
@@ -8,24 +8,38 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegate {
     
     @IBOutlet var tfNum: UITextField!
     @IBOutlet var tfName: UITextField!
     @IBOutlet var tfPassword: UITextField!
     @IBOutlet var tfPasswordAgain: UITextField!
+    @IBOutlet var scrView: UIScrollView! // 为了键盘弹起来的时候 页面上滑
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 添加点击手势
+        let gesture = UITapGestureRecognizer.init(target: self, action: #selector(hideKeyboard))
+        self.view.addGestureRecognizer(gesture)
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBarHidden = false
+        
+        // 设置监听 当键盘将要隐藏的时候 发通知给（调用）self.willHideKeyboard() 函数
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(willHideKeyboard), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        // 删除监听
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
     }
 
     @IBAction func clickToRegister(sender: UIButton) {
+        self.hideKeyboard()
         // 先判断输入的是否为空
         if tfNum.text == "" {
             
@@ -68,5 +82,32 @@ class RegisterViewController: UIViewController {
                 self.presentViewController(alert, animated: true, completion: nil)
             }
         }
+    }
+    
+    // MARK: - 控制键盘
+    
+    func hideKeyboard() {
+        if tfNum.isFirstResponder() || tfName.isFirstResponder() || tfPassword.isFirstResponder() || tfPasswordAgain.isFirstResponder() {
+            tfNum.resignFirstResponder()
+            tfName.resignFirstResponder()
+            tfPassword.resignFirstResponder()
+            tfPasswordAgain.resignFirstResponder()
+        }
+    }
+    
+    // 开始编辑的时候 设置滚动view的大小为两倍屏幕高 然后向上滚动一段距离
+    func textFieldDidBeginEditing(textField: UITextField) {
+        scrView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height * 2)
+        scrView.setContentOffset(CGPointMake(0, textField.frame.origin.y - 100), animated: true)
+    }
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        self.hideKeyboard()
+    }
+    
+    // 收起（隐藏）键盘的时候 设置滚动view为当前屏幕大小，回到 (0,0)
+    func willHideKeyboard() {
+        scrView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height)
+        scrView.setContentOffset(CGPointZero, animated: true)
     }
 }
